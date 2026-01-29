f4ce3e3d5aee16c3ca787e77cd4fc0b1f1216db5
Sereitsma <sereitsma@cosmopolitanschool.eu>
2026-01-29 14:32:32 +0100

    I have improved when it comes to the questions

--- quiz.py
+++ quiz.py
@@ -1,6 +1,6 @@
 import csv
 import types
-
+score=0
 
 def load_questions(filename):
     questions = []
@@ -12,8 +12,6 @@ def load_questions(filename):
             line['answers'] = line['answers'].split(';')
             questions.append(line)
     return questions
-
-
 questions = load_questions('quiz_questions.csv')
 
 def print_question(question):
@@ -27,7 +25,12 @@ def print_question(question):
         Option_chosen = input("Please try again!")
 
     Option_chosen = int(Option_chosen)
-
+    if Option_chosen == int(question['correct']):
+        print("Congrats, Youre answer was CORRECT!")
+        return 1
+    else:
+        print("WHOMP, WHOMP, to bad!")
+        return -1
 
 for x in questions:
     print_question(x)
